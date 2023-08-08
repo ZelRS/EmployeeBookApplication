9392633bf024d34bc1f17cf1823fa267866cb157
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-08-08 21:20:01 +0600

    добавил проверки на количество вызовов метода showEmployeeList() у мока

--- src/test/java/skypro/course2/employeeBook/service/impl/DepartmentServiceImplTest.java
+++ src/test/java/skypro/course2/employeeBook/service/impl/DepartmentServiceImplTest.java
@@ -11,7 +11,7 @@ import skypro.course2.employeeBook.service.EmployeeService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static skypro.course2.employeeBook.constants.DepartmentServiceImplTestConstants.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,42 +28,48 @@ public class DepartmentServiceImplTest {
     }
 
     //проверяет, возвращается ли сотрудник с максимальной зарплатой в отделе и выбрасывается ли исключение,
-    // если номер оттедла задан некорректно
+    //если номер оттедла задан некорректно, проверяет количество вызова метода showEmployeeList()
     @Test
     public void shouldGetEmployeeWithMaxSalaryOfDepOrThrowIfNumOfDepIncorrect() {
         assertEquals(EMPLOYEE1, out.getEmployeeWithMaxSalaryOfDep(1));
         assertThrows(DepCountException.class, () -> out.getEmployeeWithMaxSalaryOfDep(0));
+        verify(employeeServiceMock, times(1)).showEmployeeList();
     }
 
     //проверяет, возвращается ли сотрудник с минимальной зарплатой в отделе и выбрасывается ли исключение,
-    // если номер оттедла задан некорректно
+    //если номер оттедла задан некорректно, проверяет количество вызова метода showEmployeeList()
     @Test
     public void shouldGetEmployeeWithMinSalaryOfDepOrThrowIfNumOfDepIncorrect() {
         assertEquals(EMPLOYEE2, out.getEmployeeWithMinSalaryOfDep(1));
         assertThrows(DepCountException.class, () -> out.getEmployeeWithMinSalaryOfDep(0));
+        verify(employeeServiceMock, times(1)).showEmployeeList();
     }
 
     //проверяет, возвращается ли список сотрудников конкретного отдела и выбрасывается ли исключение,
-    // если номер оттедла задан некорректно
+    //если номер оттедла задан некорректно, проверяет количество вызова метода showEmployeeList()
     @Test
     public void shouldGetEmployeesListByDepOrThrowIfNumOfDepIncorrect() {
         assertEquals(CONSTANT_MAP_OF_EMPLOYEE_BY_DEP, out.getEmployeesListByDep(1));
         assertThrows(DepCountException.class, () -> out.getEmployeesListByDep(0));
+        verify(employeeServiceMock, times(1)).showEmployeeList();
     }
 
-    //проверяет, возвращается ли список всех сотрудников с разделением по отделам
+    //проверяет, возвращается ли список всех сотрудников с разделением по отделам,
+    //проверяет количество вызова метода showEmployeeList()
     @Test
     public void shouldGetAllEmployeesList() {
         assertEquals(CONSTANT_MAP_OF_EMPLOYEE, out.getAllEmployeesList());
+        verify(employeeServiceMock, times(1)).showEmployeeList();
     }
 
     //проверяет, возвращается ли сумма всех зарплат в отделе и выбрасывается ли исключение,
-    // если номер оттедла задан некорректно
+    //если номер оттедла задан некорректно, проверяет количество вызова метода showEmployeeList()
     @Test
     public void shouldGetSumOfSalaryByDepOrThrowIfNumOfDepIncorrect() {
         int expectedSum = EMPLOYEE1.getSalary() + EMPLOYEE2.getSalary();
         assertEquals(expectedSum, out.getSumOfSalaryByDep(1));
         assertThrows(DepCountException.class, () -> out.getEmployeesListByDep(0));
+        verify(employeeServiceMock, times(1)).showEmployeeList();
     }
 
 }
